8104920824efbeeafc61c1e8c12248fadd373919
David Grigoryan <d4@h3x.co>
2019-04-02 17:45:30 +0100

    Basic setup of the API layer

---
+++ api-service/src/resources/plushy-unicorns/createPlushyUnicorn.ts
@@ -0,0 +1,5 @@
+import { nonEmptyString } from 'src/utils/basic-validators';
+
+const createPlushyUnicorn = (name: any, color: any, size: any) => {
+  const isNameValid = nonEmptyString(name);
+};
---
+++ api-service/src/utils/basic-validators.ts
@@ -0,0 +1,1 @@
+export const nonEmptyString = (candidate: any): Boolean => typeof candidate === 'string' && candidate.length > 0;
